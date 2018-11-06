78c9d3e69ef4cff85d0e7119c29c868675968e71
ReAl <alokreddyk@gmail.com>
2018-11-06 21:11:25 +0530

    map json and code removed

--- spend_widget/spend.js
+++ spend_widget/spend.js
@@ -8,54 +8,6 @@ var supplierChart = dc.bubbleChart('#supplier');
 
 var numberFormat = d3.format(',.0f');
 
-//Width and height of map
-var width_spendmap = 960;
-var height_spendmap = 500;
-
-// D3 Projection
-var projection = d3.geoAlbersUsa()
-    .translate([width_spendmap / 2, height_spendmap / 2]) // translate to center of screen
-    .scale([1000]); // scale things down so see entire US
-
-// Define path generator
-var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
-    .projection(projection); // tell path generator to use albersUsa projection
-
-
-// Define linear scale for output
-var color = d3.scaleLinear()
-    .range(["rgb(213,222,217)", "rgb(69,173,168)", "rgb(84,36,55)", "rgb(217,91,67)"]);
-
-
-//Create SVG element and append map to the SVG
-var svg = d3.select("#usamap")
-    .append("svg")
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "0 0 960 500");
-
-// Append Div for tooltip to SVG
-var div = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-
-// Load GeoJSON data and merge with states data
-d3.json('us-states.json').then(function (json) {
-
-
-    // Bind the data to the SVG and create one path per GeoJSON feature
-    svg.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("stroke", "#fff")
-        .style("stroke-width", "1")
-        .style("fill", "#b3e5fc");
-
-});
-
 
 d3.json('spend.json').then(function (spend) {
     //console.log(spend);
